593f31f61523ea948505e2f527402904c114cbf5
Edgaru089 <sjs333@outlook.com>
2018-07-08 13:06:10 +0800

    Bumped version to 1.2.1

--- UserServer/Main.cpp
+++ UserServer/Main.cpp
@@ -29,7 +29,7 @@ BOOL systemExitEventHandler(DWORD dwCtrlType) {
 
 #endif // SFML_SYSTEM_WINDOWS
 
-const string versionString = "1.2";
+const string versionString = "1.2.1";
 const string compileTime = string(__DATE__) + " " + string(__TIME__);
 
 ofstream logOut("latest.log");
